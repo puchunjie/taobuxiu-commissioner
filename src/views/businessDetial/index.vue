5e55bedbae869e36af7b9d2d6d080dc7cb438560
well kim <wellkim@welldeMacBook-Pro.local>
2017-11-13 14:00:27 +0800

    登陆、商户列表

--- src/views/businessDetial/index.vue
+++ src/views/businessDetial/index.vue
@@ -1,24 +1,119 @@
 <template>
   <div>
-    <publicHead>标题</publicHead>
-    商家详情
+    <publicHead>商户详情</publicHead>
+    <div class="content">
+      <ul class="businessDetail">
+        <li class="column"><div class="column-left">公司名称</div><div class="column-right">{{list.companyName}}</div></li>
+        <li class="column">
+          <div class="column-left">资质认证</div>
+          <div class="column-right">
+            <span v-show="list.isFaithUser == '1'" class="iconfont icon-chengxin"></span>
+            <span v-show="list.isGuaranteeUser == '1'" class="iconfont icon-bao"></span>
+          </div>
+        </li>
+        <li class="column"><div class="column-left">注册手机</div><div class="column-right">{{tel}}</div></li>
+        <li class="column"><div class="column-left">入驻时间</div><div class="column-right">{{list.beBuserTime | dateformat}}</div></li>
+        <li class="column"><div class="column-left">注册资金</div><div class="column-right">{{list.regMoney}}{{list.unit}}</div></li>
+        <li class="column"><div class="column-left">联系人</div><div class="column-right">{{list.contact}}</div></li>
+        <li class="column"><div class="column-left">联系方式</div><div class="column-right">{{list.contactNum}}</div></li>
+        <li class="column"><div class="column-left">公司地址</div><div class="column-right">{{list.address}}</div></li>
+        <li class="column"><div class="column-left">优惠信息</div><div class="column-right">{{list.proInfo | emptyHlod(payload="暂无")}}</div></li>
+        <li class="column"><div class="column-left">仓库信息</div><div class="column-right">{{list.storeHouseName | emptyHlod(payload="暂无")}}</div></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import publicHead from '@/components/header'
+import publicHead from "@/components/header";
+
 export default {
   components: {
-    publicHead
+    publicHead,
   },
-  data () {
+  data() {
     return {
-
+      list: [],
+      tel: ''
+    };
+  },
+  computed: {
+    id() {
+        return this.$route.params.id
     }
+  },
+  methods: {
+    getBusinessDetail(){
+      this.$http.get('sys/salesManBuserInfo/findBuserInfo',{
+        params:{
+          userId: this.id
+        }
+      }).then(res =>{
+        if(res.code === 1000){
+          this.list = res.data.buserInfo;
+          this.tel = res.data.mobile
+        }
+      })
+    }
+  
+  },
+  created() {
+    this.getBusinessDetail();
   }
-}
+};
 </script>
 <style lang="less" scoped>
-  
+@import '../../style/mixin.less'; 
+.content {
+  padding: 0.5rem 0;
+}
+.businessDetail{
+  background-color: #fff;
+  .column{
+    position: relative;
+    font-size: .16rem;
+    padding: .12rem 0;
+    .flex-block;
+    &:before {
+      content: '';
+        position: absolute;
+        left: .16rem;
+        bottom: 0;
+        right: 0;
+        height: 1px;
+        border-top: 1px solid @line_light_gray;
+        display: block;
+        transform: scaleY(0.5);
+        transform-origin: 0 100%;
+        pointer-events: none;
+      }
+      &:last-child::before{
+        display: none;
+      }
+      .column-left {
+        position: relative;
+        padding: 0 .16rem;
+        color: #999;
+        min-width: 1rem;
+        display: -webkit-box;
+        box-pack: start;
+        box-align: center;
+      }
+      .column-right {
+        position: relative;
+        padding-right: .16rem;
+        color: #333;
+        .iconfont{
+          font-size: .16rem;
+          &.icon-chengxin{
+            color: #f5a623;
+          }
+          &.icon-bao{
+            color: #c16bd6;
+          }
+        }
+      }
+    }
+}
 </style>
 
--- src/views/businessList/index.vue
+++ src/views/businessList/index.vue
@@ -4,9 +4,19 @@
     <div class="mid-container">
       <search v-model="text"></search>
       <div class="item-list">
-        <scroller :on-refresh="refresh" :on-infinite="infinite" refresh-layer-color="#4b8bf4"
-      loading-layer-color="#ec4949">
-          <p v-for="el in 20" :key="el">{{ el }}</p>
+        <scroller ref="scroller" :on-refresh="refresh" :on-infinite="infinite" refresh-layer-color="#4b8bf4" loading-layer-color="#ec4949">
+          <div class="list-column">
+            <div class="list" v-for="item in list" :key="item.userId">
+              <div class="list-main" @click="$router.push('/businessDetial-'+item.userId)">
+                <div class="name">{{item.companyName}}</div>
+                <div class="address">{{item.address}}</div>
+              </div>
+              <div class="list-info">
+                <span><i class="iconfont icon-yonghu"></i>{{item.contact}}</span>
+                <span class="tel"><i class="iconfont icon-dianhua"></i><a :href="'tel:' + item.contactNum">{{item.contactNum}}</a></span>
+              </div>
+            </div>
+          </div>
         </scroller>
       </div>
     </div>
@@ -15,49 +25,176 @@
 </template>
 
 <script>
-  import publicHead from '@/components/header'
-  import bottomTab from '@/components/bottomTab'
-  import search from '@/components/search'
-  export default {
-    components: {
-      bottomTab,
-      publicHead,
-      search
-    },
-    data() {
-      return {
-        text: ''
-      }
-    },
-    methods: {
-      refresh(done) {
-        setTimeout(() => {
-          console.log("刷新")
-          done()
-        }, 1500)
+import publicHead from "@/components/header";
+import bottomTab from "@/components/bottomTab";
+import search from "@/components/search";
+export default {
+  components: {
+    bottomTab,
+    publicHead,
+    search
+  },
+  data() {
+    return {
+      text: "",
+      list: [],
+      apiData: {
+        currentPage: 1,
+        pageSize: 10,
+        queryMessage: ""
       },
-      infinite(done) {
-        setTimeout(() => {
-          console.log("加载")
-          done()
-        }, 1500)
-      }
+      maxCount: 0
+    };
+  },
+  computed: {
+    max() {
+      return Math.ceil(this.maxCount / this.apiData.pageSize);
     }
+  },
+  methods: {
+    //  刷新
+    refresh(done) {
+        this.apiData.currentPage = 1;
+        this.getList();
+        done();
+    },
+    //  加载更多
+    infinite(done) {
+        if (this.apiData.currentPage < this.max) {
+          this.apiData.currentPage++;
+          this.getList();
+          done();
+        }else{
+          done(true);
+        }
+    },
+    //  获取列表
+    getList() {
+      this.$http.get("sys/salesManBuserInfo/findBuserInfoPage",{
+            params: this.apiData
+          }
+        ).then(res => {
+          // console.log(res);
+          if (res.code === 1000) {
+            if (this.apiData.currentPage === 1) {
+              this.list = res.data.list;
+            } else {
+              this.list.push(...res.data.list);
+            }
+            this.maxCount = res.data.totalCount;
+          }
+        });
+    },
+    // 设置滚动条
+    setScroll(){
+      this.$nextTick(()=> {
+        setTimeout(() => {
+          this.$refs.scroller.scrollTo(0, this.scrollTop);
+        }, 500);
+      })
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    // 如果是从详情返回，则设置滚动到原来位置
+    next(vm => {
+      if (from.name == 'businessDetial') {
+        vm.setScroll();
+      }
+    })
+  },
+  beforeRouteLeave(to, from, next) {
+    // 如果是进入详情页，则保存滚动位置,如果是其他页面初始化所有数据
+    if (to.name == 'businessDetial') {
+      this.scrollTop = this.$refs.scroller.getPosition().top;
+    }
+    next();
+  },
+  created() {
+    this.getList();
   }
+};
 </script>
 
 <style lang="less" scoped>
-  .mid-container {
-    width: 100%;
-    height: 100%;
-    padding: .4rem 0 .5rem 0;
-    .item-list{
-      position: relative;
-      height: calc(~"100% - .4rem");
-      p{
-        line-height: .3rem;
+@import "../../style/mixin.less";
+.mid-container {
+  width: 100%;
+  height: 100%;
+  padding: 0.4rem 0 0.5rem 0;
+  .item-list {
+    position: relative;
+    height: calc(~"100% - .4rem");
+    p {
+      line-height: 0.3rem;
+    }
+  }
+  .list-column {
+    padding: 0.1rem 0.09rem;
+    .list {
+      background: #fff;
+      margin-bottom: 0.04rem;
+      .list-main {
+        position: relative;
+        padding: 0.15rem;
+        padding-right: 0.3rem;
+        &:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          right: 0;
+          height: 1px;
+          border-top: 1px solid @line_light_gray;
+          display: block;
+          transform: scaleY(0.5);
+          transform-origin: 0 100%;
+          pointer-events: none;
+        }
+        &:after {
+          content: "";
+          position: absolute;
+          right: 0.15rem;
+          top: calc(50% - 0.05rem);
+          width: 0.1rem;
+          height: 0.1rem;
+          border-top: 1px solid #999;
+          border-right: 1px solid #999;
+          transform: rotate(45deg);
+        }
+        .name {
+          font-size: 0.17rem;
+          color: #333;
+          .ellipsis;
+        }
+        .address {
+          font-size: 0.14rem;
+          color: #666;
+          .ellipsis;
+        }
+      }
+      .list-info {
+        padding: 0.15rem;
+        font-size: 0.15rem;
+        color: #666;
+        span {
+          display: inline-block;
+          margin-right: 0.1rem;
+          &.tel {
+            color: @base_blue;
+            border-left: 1px solid @line_light_gray;
+            padding-left: 0.1rem;
+          }
+          i {
+            margin-right: 0.1rem;
+          }
+        }
+        .iconfont {
+          color: @base_blue;
+          font-size: 0.15rem;
+        }
       }
     }
   }
+}
 </style>
 
